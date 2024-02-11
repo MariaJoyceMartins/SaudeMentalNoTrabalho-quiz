270657d3897a65947a3ade302786c289db15a328
MariaJoyceMartins <159647621+MariaJoyceMartins@users.noreply.github.com>
2024-02-11 14:15:24 -0300

    aprimoramento

--- index.js
+++ index.js
@@ -6,52 +6,103 @@ const perguntas = [
       'Promover uma cultura de competição extrema',
       'Fornecer treinamento em habilidades de enfrentamento do estresse',
     ],
-    correta: 1,
+    correta: 1
   },
-  // ... outras perguntas
+  {
+      pergunta: 'Qual é o papel da liderança na promoção da saúde mental no local de trabalho?',
+    respostas: [
+      'Promover uma cultura de apoio e respeito',
+      'Ignorar os sinais de estresse dos funcionários',
+      'Aumentar a pressão e a competitividade',
+    ],
+    correta: 0
+  },
+  {
+    pergunta: 'Como a integração de atividades sociais no local de trabalho pode beneficiar a saúde mental dos colaboradores?',
+    respostas: [
+      'Isolando os funcionários para que foquem apenas no trabalho',
+      'Criando um ambiente de apoio',
+      'Aumentando a competição entre colegas',
+    ],
+    correta: 1
+  },
+  {
+    pergunta: 'Quais são os possíveis efeitos de uma comunicação aberta e transparente no ambiente de trabalho?',
+    respostas: [
+      'Aumento da confiança e conflitos',
+      'Aumento da competição e individualismo',
+      'Fomento da colaboração e resolução de problemas',
+    ],
+    correta: 2
+  },
+  {
+    pergunta: 'Qual desses fatores pode contribuir para um ambiente de trabalho mais inclusivo e saudável?',
+    respostas: [
+      'Promover apenas uma perspectiva única e dominante',
+      'Valorizar a diversidade de pensamento e experiência',
+      'Excluir qualquer voz discordante',
+    ],
+    correta: 1
+  },
+  {
+    pergunta: 'Como a implementação de programas de mindfulness pode impactar a saúde mental no trabalho?',
+    respostas: [
+      'Aumentando o estresse e ansiedade dos funcionários',
+      'Desestimulando a autoconsciência e o autocuidado',
+      'Fornecendo ferramentas para lidar com desafios e pressões',
+    ],
+    correta: 2
+  },
+  {
+    pergunta: 'Qual é uma forma eficaz de lidar com o conflito no local de trabalho para promover a saúde mental?',
+    respostas: [
+      'Ignorar o conflito e esperar que ele se resolva',
+      'Encorajar a comunicação aberta e a resolução colaborativa',
+      'Promover a competição entre os colegas',
+    ],
+    correta: 1
+  },
 ];
 
-const quiz = document.querySelector('#quiz');
-const template = document.querySelector('template');
+const quiz = document.querySelector('#quiz')
+const template = document.querySelector('template')
+const quizItem = template.content.cloneNode(true)
+quiz.appendChild(quizItem)
 
-// Controle das respostas
-let corretas = new Set();
-const totalDePerguntas = perguntas.length;
-const mostrarTotal = document.querySelector('#acertos span');
-mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas;
+const corretas = new Set()
+const totalDePerguntas = perguntas.length
+const mostrarTotal = document.querySelector('#acertos span')
+mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
 
-// Loop para exibir as perguntas
-for (const item of perguntas) {
-  const quizItem = template.content.cloneNode(true);
-  quizItem.querySelector('h3').textContent = item.pergunta;
+for(const item of perguntas) {
+const quizItem = template.content.cloneNode(true)
+quizItem.querySelector('h3').textContent = item.pergunta
 
-  // Loop para exibir as opções de resposta
-  for (let resposta of item.respostas) {
-    const dt = quizItem.querySelector('dl dt').cloneNode(true);
-    dt.querySelector('span').textContent = resposta;
+for(let resposta of item.respostas ) {
+const dt = quizItem.querySelector('dl dt').cloneNode(true)
+dt.querySelector('span').textContent = resposta
 
-    // Atributos do input
-    dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item));
-    dt.querySelector('input').value = item.respostas.indexOf(resposta);
+dt.querySelector('input').setAttribute('name', 'pergunta-' + 
+perguntas.indexOf(item))
 
-    // Atualização das respostas
-    dt.querySelector('input').onchange = (event) => {
-      const estaCorreta = event.target.value == item.correta;
+dt.querySelector('input').value = item.respostas.indexOf(resposta)
 
-      corretas.delete(item);
-      if (estaCorreta) {
-        corretas.add(item);
-      }
+dt.querySelector('input').onchange = (event) => {
+const estaCorreta = event.target.value == item.correta
 
-      mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas;
-    };
+corretas.delete(item)
+if(estaCorreta){
+corretas.add(item) 
+}
 
-    quizItem.querySelector('dl').appendChild(dt);
-  }
+mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+}
 
-  // Remoção da primeira opção (apenas para corrigir a ordem)
-  quizItem.querySelector('dl dt:first-child').remove();
+quizItem.querySelector('dl').appendChild(dt)
+}
 
-  // Exibição da pergunta
-  quiz.appendChild(quizItem);
+quizItem.querySelector('dl dt').remove()
+
+quiz.appendChild(quizItem)
+
 }
